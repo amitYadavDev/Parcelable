c073556f5d49c1d25e29ebabc8dd1d640fbbf9c6
amitYadavDev <amit8955461017@gmail.com>
2024-01-31 11:12:48 +0530

    passing data b/w activity using parcel

--- app/src/main/java/amitapps/media/parcelable/MainActivity.kt
+++ app/src/main/java/amitapps/media/parcelable/MainActivity.kt
@@ -1,16 +1,27 @@
 package amitapps.media.parcelable
 
 import amitapps.media.parcelable.R.id.btn
+import amitapps.media.parcelable.databinding.ActivityMainBinding
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btn: Button = findViewById(R.id.btn)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("data", DataParcelable("Amit", 24))
+            startActivity(intent)
+        }
     }
 }
